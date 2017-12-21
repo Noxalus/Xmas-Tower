90bea727b4501a5cb2f291d4d6f20ea59ff5523b
Noxalus <noxalus@gmail.com>
2017-12-21 14:55:28 +0100

    Add a script to deploy Android signed APK to HockeyApp.

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd ${DIR}/.. > /dev/null
+
+SIGNED_APK="XmasTower.apk"
+
+echo "Deploy signed APK on HockeyApp"
+
+curl -F "status=2" -F "notify=1" -F "notes=$TRAVIS_COMMIT_MESSAGE" -F "notes_type=0" -F "ipa=@$SIGNED_APK" -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" https://rink.hockeyapp.net/api/2/apps/upload
+
+popd > /dev/null
